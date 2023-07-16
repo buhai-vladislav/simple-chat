92a346ca1229a8c73bd4b90b0dc567bb1703aa0f
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-16 07:39:31 +0300

    create error handling hook

---
+++ src/client/src/hooks/useErrorToast.ts
@@ -0,0 +1,33 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+import { useEffect } from 'react';
+import { toast, ToastOptions } from 'react-toastify';
+import { HttpStatus } from '../types/HttpStatus';
+import { IResponse } from '../types/Response';
+
+type HooksError = FetchBaseQueryError | SerializedError | undefined;
+
+export interface IErrorItem {
+  status: HttpStatus;
+  errorMessage?: string;
+  callback?: () => void;
+}
+
+export function useErrorToast(
+  error: HooksError,
+  errorCondition: IErrorItem[],
+  toastOptions?: ToastOptions,
+) {
+  useEffect(() => {
+    if (error && 'data' in error) {
+      const { statusCode, message } = error.data as Partial<IResponse<unknown>>;
+
+      errorCondition.forEach(({ status, errorMessage, callback }) => {
+        if (message && statusCode && statusCode === status) {
+          toast(errorMessage ?? message, toastOptions);
+          callback && callback();
+        }
+      });
+    }
+  }, [error]);
+}
